5fc147d1a1bfd78d7fc7aa1402e807b9031527c7
Jschles1 <jschles1@ramapo.edu>
2021-03-23 18:46:02 -0400

    Add functionality for fetching posts w/ SWR

---
+++ src/data/hooks/usePosts.js
@@ -0,0 +1,29 @@
+import useSWR, { mutate } from 'swr';
+import fetcher from '../fetcher';
+
+const baseURL = 'https://jsonplaceholder.typicode.com/posts';
+
+const usePosts = () => {
+  const { data, error } = useSWR(baseURL, fetcher);
+
+  const update = (newPost) => {
+    mutate(baseURL, async (posts) => {
+      const updatedPost = await fetch(`/api/todos/${newPost.id}`, {
+        method: 'PATCH',
+        body: JSON.stringify(newPost),
+      });
+
+      const filteredPosts = posts.filter((post) => post.id !== newPost.id);
+      return [...filteredPosts, updatedPost];
+    }, false);
+  };
+
+  return {
+    posts: data,
+    loading: !error && !data,
+    error,
+    update,
+  };
+};
+
+export default usePosts;
