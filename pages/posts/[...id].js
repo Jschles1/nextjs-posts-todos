cc012e98ac038d0520ab2150d9f2c1585a7dec8c
Jschles1 <jschles1@ramapo.edu>
2021-03-26 19:36:03 -0400

    Add react-hook-form, update post detail for editing

--- pages/posts/[...id].js
+++ pages/posts/[...id].js
@@ -1,7 +1,8 @@
+import { useState } from 'react';
 import { useRouter } from 'next/router';
-import Image from 'next/image';
+import { useForm } from 'react-hook-form';
 import { makeStyles } from '@material-ui/core/styles';
-import { Card, CardHeader, CardMedia, CardActions, CardActionArea, CardContent, Button } from '@material-ui/core';
+import { Card, CardHeader, CardMedia, CardActions, CardActionArea, CardContent, Button, TextField } from '@material-ui/core';
 import LoadingSpinner from '../../src/components/LoadingSpinner';
 import usePosts from '../../src/data/hooks/usePosts';
 
@@ -13,6 +14,19 @@ const useStyles = makeStyles((theme) => ({
     margin: '0 auto',
     width: 700,
   },
+  headerRoot: {
+    padding: theme.spacing(2),
+    fontSize: 24,
+  },
+  inputRoot: {
+    width: '100%',
+  },
+  titleInputText: {
+    fontSize: 24,
+  },
+  bodyInputText: {
+    fontSize: 13.3333,
+  },
   media: {
     height: 300,
     width: 700,
@@ -20,47 +34,138 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const Post = () => {
-  const { query } = useRouter();
-  const { posts, error, loading } = usePosts();
+  const { register, handleSubmit, setValue, reset } = useForm({
+    mode: 'onSubmit',
+    reValidateMode: 'onBlur',
+  });
+  const { query, push } = useRouter();
+  const { posts, error, loading, updatePost, deletePost } = usePosts();
   const classes = useStyles();
-  console.log('q', query);
+  const [isEditing, setIsEditing] = useState(false);
   const id = query.id && parseInt(query.id[0]);
 
   if (loading) {
     return <LoadingSpinner />;
   }
 
+  console.log('posts', posts);
+
+  if (!posts) {
+    return null;
+  }
+
   const post = posts.find((p) => p.id === id);
   const { title, body } = post;
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      setValue('title', title);
+      setValue('body', body);
+    }, 0);
+  };
+
+  const handleDeleteClick = async () => {
+    await deletePost(id);
+    push('/posts');
+  };
+
+  const handleCancelClick = () => {
+    setIsEditing(false);
+    reset();
+  };
+
+  const handleOnSubmit = async (data) => {
+    const { title, body } = data;
+    const newPost = { ...post, title, body };
+    await updatePost(newPost);
+    push('/posts');
+  };
+  
+  const cardOptions = () => {
+    return isEditing ? (
+      <>
+        <Button
+          color="primary"
+          size="small"
+          onClick={handleCancelClick}
+        >
+          Cancel
+        </Button>
+
+        <Button
+          size="small"
+          onClick={handleSubmit(handleOnSubmit)}
+        >
+          Submit
+        </Button>
+      </>
+    ) : (
+      <>
+        <Button
+          color="primary"
+          size="small"
+          onClick={handleEditClick}
+        >
+          Edit
+        </Button>
+
+        <Button
+          size="small"
+          onClick={handleDeleteClick}
+        >
+          Delete
+        </Button>
+      </>
+    );
+  };
   
   return (
     <div className={classes.root}>
       <Card className={classes.card}>
-        <CardHeader title={title} />
+        <CardHeader
+          component={() => (
+            <div className={classes.headerRoot}>
+              {isEditing ? (
+                <TextField
+                  inputRef={register({ required: true })}
+                  classes={{ root: classes.inputRoot }}
+                  multiline={true}
+                  InputProps={{
+                    classes: { root: classes.titleInputText },
+                  }}
+                  name="title"
+                />
+              ) : (
+                `${title}`
+              )}
+            </div>
+          )}
+        />
         <CardActionArea>
           <CardMedia
             image="https://picsum.photos/700/300"
             className={classes.media}
           />
           <CardContent>
-            {body}
+            {isEditing ? (
+              <TextField
+                inputRef={register({ required: true })}
+                classes={{ root: classes.inputRoot }}
+                multiline={true}
+                InputProps={{
+                  classes: { root: classes.bodyInputText },
+                }}
+                name="body"
+              />
+            ) : (
+              `${body}`
+            )}
           </CardContent>
         </CardActionArea>
         
         <CardActions>
-          <Button
-            color="primary"
-            size="small"
-          >
-            Edit
-          </Button>
-
-          <Button
-            color="error"
-            size="small"
-          >
-            Delete
-          </Button>
+          {cardOptions()}
         </CardActions>
       </Card>
     </div>
